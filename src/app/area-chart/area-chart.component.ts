653bcffcec3140bd6734616ef3a37237e920e081
deaneturner <service@deaneturner.com>
2022-09-20 15:45:36 -0400

    rendering area chart

--- src/app/area-chart/area-chart.component.ts
+++ src/app/area-chart/area-chart.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewEncapsulation } from '@angular/core';
 
 import * as d3 from 'd3';
+import { ScaleLinear, ScaleOrdinal } from 'd3';
 
 @Component({
   selector: 'app-area-chart',
@@ -9,9 +10,26 @@ import * as d3 from 'd3';
   styleUrls: ['./area-chart.component.scss']
 })
 export class AreaChartComponent implements OnInit, OnChanges {
+  @Input() xmax = 45;
+  @Input() ymax = 200;
+  @Input() hticks = 60;
   @Input() data: number[] | undefined;
+  @Input() showLabel = 1;
+
   hostElement; // Native element hosting the SVG container
-  svg: d3.Selection<SVGSVGElement, unknown, null, undefined> | undefined; // Top level SVG element
+  svg!: d3.Selection<SVGSVGElement, unknown, null, undefined>; // Top level SVG element
+  g!: any;
+  colorScale!: ScaleOrdinal<string, string>;
+  // @ts-ignore
+  x!;
+  y!: ScaleLinear<number, number, never>;
+  bins!: d3.Bin<number, number>[][]; // Array of frequency distributions - one for each area chart
+  paths!: any[];
+  // @ts-ignore
+  area!; // For D3 area function
+  // For D3 area function
+  // For D3 area function
+  histogram!: d3.HistogramGeneratorNumber<number, number>; // For D3 histogram function
 
   constructor(private elRef: ElementRef) {
     this.hostElement = this.elRef.nativeElement;
@@ -38,6 +56,31 @@ export class AreaChartComponent implements OnInit, OnChanges {
     this.removeExistingChartFromParent();
 
     this.setChartDimensions();
+
+    this.setColorScale();
+
+    this.addGraphicsElement();
+
+    this.createXAxis();
+
+    this.createYAxis();
+
+    // d3 area and histogram functions have to be declared after x and y functions are defined
+    this.area = d3.area()
+      .x((datum: any) => this.x(d3.mean([datum.x1, datum.x2])))
+      .y0(this.y(0))
+      .y1((datum: any) => this.y(datum.length));
+
+    this.histogram = d3.histogram()
+      .value((datum) => datum)
+      .domain([0, this.xmax])
+      .thresholds(this.x.ticks(this.hticks));
+
+    // data has to be processed after area and histogram functions are defined
+    this.processData(data);
+
+    this.createAreaCharts();
+
   }
 
   private setChartDimensions() {
@@ -49,11 +92,84 @@ export class AreaChartComponent implements OnInit, OnChanges {
       .attr('viewBox', '0 0 ' + viewBoxWidth + ' ' + viewBoxHeight);
   }
 
+  private setColorScale() {
+    this.colorScale = d3.scaleOrdinal(d3.schemeCategory10);
+    // Below is an example of using custom colors
+    // this.colorScale = d3.scaleOrdinal().domain(["0","1","2","3"]).range(['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728']);
+  }
+
   private removeExistingChartFromParent() {
     // !!!!Caution!!!
     // Make sure not to do;
     //     d3.select('svg').remove();
     // That will clear all other SVG elements in the DOM
     d3.select(this.hostElement).select('svg').remove();
+  }
+
+  private addGraphicsElement() {
+    this.g = this.svg.append('g')
+      .attr('transform', 'translate(0,0)');
+  }
+
+  private createXAxis() {
+    this.x = d3.scaleLinear()
+      .domain([0, this.xmax])
+      .range([30, 170]);
+
+    this.g.append('g')
+      .attr('transform', 'translate(0,90)')
+      .attr('stroke-width', 0.5)
+      .call(d3.axisBottom(this.x).tickSize(0).tickFormat('' as any));
+
+    this.g.append('g')
+      .attr('transform', 'translate(0,90)')
+      .style('font-size', '6')
+      .style('stroke-dasharray', ('1,1'))
+      .attr('stroke-width', 0.1)
+      .call(d3.axisBottom(this.x).ticks(10).tickSize(-80));
+  }
+
+  private createYAxis() {
+    this.y = d3.scaleLinear()
+      .domain([0, this.ymax])
+      .range([90, 10]);
+    this.g.append('g')
+      .attr('transform', 'translate(30,0)')
+      .attr('stroke-width', 0.5)
+      .call(d3.axisLeft(this.y).tickSize(0).tickFormat('' as any));
+    this.g.append('g')
+      .attr('transform', 'translate(30,0)')
+      .style('stroke-dasharray', ('1,1'))
+      .attr('stroke-width', 0.1)
+      .call(d3.axisLeft(this.y).ticks(4).tickSize(-140))
+      .style('font-size', '6');
+
+    if (this.showLabel === 1) {
+      this.g.append('text')
+        .attr('text-anchor', 'middle')
+        .attr('transform', 'translate(10,50) rotate(-90)')
+        .style('font-size', 8)
+        .text('Frequency');
+    }
+  }
+
+  private processData(data: number[]) {
+    this.bins = [];
+    data.forEach((row) => {
+      this.bins.push(this.histogram(row as any))
+    });
+  }
+
+  private createAreaCharts() {
+    this.paths = [];
+    this.bins.forEach((row, index) => {
+      this.paths.push(this.g.append('path')
+        .datum(row)
+        .attr('fill', this.colorScale('' + index))
+        .attr('stroke-width', 0.1)
+        .attr('opacity', 0.5)
+        .attr('d', (datum: any) => this.area(datum))
+      );
+    });
   }
 }
